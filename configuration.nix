c05e2cc0ccc4d070e0fb0acd680c67679fffb3c8
Xamcost <maxime.costalonga@gmail.com>
2024-07-13 02:12:39 +0100

    feat: add lazygit

--- configuration.nix
+++ configuration.nix
@@ -103,9 +103,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     git
+    pkgs.lazygit
     inputs.nixpkgs.legacyPackages.${pkgs.system}.neovim
     pkgs.home-manager
   ];
