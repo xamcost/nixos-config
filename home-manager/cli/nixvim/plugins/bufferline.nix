4a1d6bf607c20253ae54298183769a316a0c4c7a
Xamcost <maxime.costalonga@gmail.com>
2024-07-14 16:41:01 +0100

    feat: add buffer delete command

--- home-manager/cli/nixvim/plugins/bufferline.nix
+++ home-manager/cli/nixvim/plugins/bufferline.nix
@@ -16,6 +16,14 @@
     keymaps = [
       {
         mode = "n";
+        key = "<leader>bd";
+        action = "<cmd>bdelete<cr>";
+        options = {
+          desc = "Delete buffer";
+        };
+      }
+      {
+        mode = "n";
         key = "<leader>bb";
         action = "<cmd>e #<cr>";
         options = {
