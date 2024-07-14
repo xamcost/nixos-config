dcbb3753c20a1939e025c82c4097af60b70b440d
Xamcost <maxime.costalonga@gmail.com>
2024-07-14 11:04:48 +0100

    feat: add separate neotree config

--- home-manager/nixvim/plugins/neo-tree.nix
+++ home-manager/nixvim/plugins/neo-tree.nix
@@ -1,6 +1,22 @@
 {
-  programs.nixvim.plugins.neo-tree = {
-    enable = true;
-    closeIfLastWindow = true;
+  programs.nixvim = {
+    plugins.neo-tree = {
+      enable = true;
+
+      closeIfLastWindow = true;
+      window = {
+        width = 30;
+        autoExpandWidth = true;
+      };
+    };
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>n";
+        action = ":Neotree action=focus reveal toggle<CR>";
+        options.silent = true;
+      }
+    ];
   };
 }
