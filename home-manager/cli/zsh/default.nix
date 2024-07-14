9ea82afa4738ceca1015bddd456a33a9208d25cc
Xamcost <maxime.costalonga@gmail.com>
2024-07-14 16:25:32 +0100

    fix: add dotdir to zsh

--- home-manager/cli/zsh/default.nix
+++ home-manager/cli/zsh/default.nix
@@ -1,15 +1,19 @@
 {
   programs.zsh = {
     enable = true;
+    dotDir = ".config/zsh";
+
     autosuggestion.enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
+    
     history = {
       size = 50000;
       extended = true;
       expireDuplicatesFirst = true;
       ignoreAllDups = true;
     };
+
     shellAliases = {
       ".." = "cd ..";
       "..." = "cd ../..";
