872ae3dee1e1d0775990d1fdbcc9e432f0593b03
Xamcost <maxime.costalonga@gmail.com>
2024-08-17 02:09:54 +0100

    maint(immich): update to v1.112.1

--- services/immich.nix
+++ services/immich.nix
@@ -17,7 +17,7 @@
 
   # Containers
   virtualisation.oci-containers.containers."immich_server" = {
-    image = "ghcr.io/immich-app/immich-server:v1.111.0";
+    image = "ghcr.io/immich-app/immich-server:v1.112.1";
     environment = {
       TZ = "Europe/London";
     };
@@ -62,7 +62,7 @@
     ];
   };
   virtualisation.oci-containers.containers."immich_machine_learning" = {
-    image = "ghcr.io/immich-app/immich-machine-learning:v1.111.0";
+    image = "ghcr.io/immich-app/immich-machine-learning:v1.112.1";
     environment = {
       TZ = "Europe/London";
     };
