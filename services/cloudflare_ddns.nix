c5b643c03748c2bb1184d92573e3b69c35f2b117
Xamcost <maxime.costalonga@gmail.com>
2024-09-15 18:25:19 +0100

    maint: replace ddns container by nixos service

--- services/cloudflare_ddns.nix
+++ services/cloudflare_ddns.nix
@@ -1,23 +1,17 @@
-{ config, ... }:
-{
+{config, ...}: {
   sops.secrets.cloudflare-ddns-token = {};
   sops.secrets.cloudflare-zoneid = {};
   sops.secrets.home-prefix = {};
   sops.secrets.domain = {};
 
   sops.templates."cloudflare-ddns.env".content = ''
-    CF_DNS__DOMAINS_0__ZONE_ID=${config.sops.placeholder.cloudflare-zoneid}
-    CF_DNS__AUTH__SCOPED_TOKEN=${config.sops.placeholder.cloudflare-ddns-token}
-    CF_DNS__DOMAINS_0__NAME=${config.sops.placeholder.home-prefix}.${config.sops.placeholder.domain}
-    CF_DNS__CRON="*/15 * * * *"
-    CF_DNS__DOMAINS_0__TYPE=A
-    CF_DNS__DOMAINS_0__PROXIED=false
+        CLOUDFLARE_API_TOKEN="${config.sops.placeholder.cloudflare-ddns-token}"
+        CLOUDFLARE_DOMAINS="${config.sops.placeholder.home-prefix}.${config.sops.placeholder.domain}"
   '';
 
-  virtualisation.oci-containers.containers."cloudflare_ddns" = {
-    image = "ghcr.io/joshuaavalon/cloudflare-ddns:latest";
-    environmentFiles = [
-      config.sops.templates."cloudflare-ddns.env".path
-    ];
+  services.cloudflare-dyndns = {
+    enable = true;
+    frequency = "*:0/15";
+    apiTokenFile = config.sops.templates."cloudflare-ddns.env".path;
   };
 }
