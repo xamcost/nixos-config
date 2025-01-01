ed5e8657b05f82a38d22593409ce95741bd71415
Maxime Costalonga <maxime.costalonga.external@atos.net>
2025-01-01 12:38:22 +0000

    feat: add minimal starship config

--- home-manager/cli/default.nix
+++ home-manager/cli/default.nix
@@ -1,5 +1,6 @@
 {inputs, ...}: {
   imports = [
+    ./starship
     ./tmux
     ./zsh
   ];
---
+++ home-manager/cli/starship/default.nix
@@ -0,0 +1,16 @@
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      palette = "tokyonight";
+      palettes.tokyonight = {
+        red = "#c53b53";
+        green = "#c3e88d";
+        blue = "#7dcfff";
+        orange = "#ff9e64";
+        yellow = "#ffc777";
+        purple = "#9d7cd8";
+      };
+    };
+  };
+}
