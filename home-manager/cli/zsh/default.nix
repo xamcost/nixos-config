c594e06fbb94efd522630ca7826b78686e0870b7
Maxime Costalonga <maxime.costalonga.external@eviden.com>
2025-01-02 16:21:51 +0000

    feat(zsh): improve word navigation and history search

--- home-manager/cli/zsh/default.nix
+++ home-manager/cli/zsh/default.nix
@@ -21,5 +21,17 @@
       "lla" = "ls -lah";
       "lg" = "lazygit";
     };
+
+    initExtra = ''
+      bindkey "^[[1;3C" forward-word
+      bindkey "^[[1;3D" backward-word
+      bindkey "^[[1;5D" beginning-of-line
+      bindkey "^[[1;5C" end-of-line
+      autoload -U history-search-end
+      zle -N history-beginning-search-backward-end history-search-end
+      zle -N history-beginning-search-forward-end history-search-end
+      bindkey "^[[A" history-beginning-search-backward-end
+      bindkey "^[[B" history-beginning-search-forward-end
+    '';
   };
 }
