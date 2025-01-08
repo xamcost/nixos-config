19c1f40212e3967b327203eb248ef33366c5a40f
Maxime Costalonga <maxime.costalonga.external@eviden.com>
2025-01-08 01:28:12 +0000

    feat(mac-work): add opentofu and newman

--- hosts/xam-mac-work/configuration.nix
+++ hosts/xam-mac-work/configuration.nix
@@ -12,6 +12,9 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    k2tf  # to convert Kubernetes resources to Terraform
+    newman  # to run Postman collections from the command line
+    opentofu
     postgresql_15
   ];
 
