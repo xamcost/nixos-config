e3595f20a330cca48485c86b2c5d4a64e36c5aa2
Maxime Costalonga <maxime.costalonga.external@eviden.com>
2025-03-07 16:35:37 +0000

    feat(nixvim): add dashboard

--- home-manager/nixvim/plugins/snacks.nix
+++ home-manager/nixvim/plugins/snacks.nix
@@ -1,3 +1,14 @@
+{ pkgs, ... }:
+  let
+    commonPane2Section = {
+      pane = 2;
+      section = "terminal";
+      enabled.__raw = ''Snacks.git.get_root() ~= nil'';
+      padding = 1;
+      ttl = 5 * 60;
+      indent = 3;
+    };
+  in
 {
   programs.nixvim = {
     plugins.snacks = {
@@ -14,6 +25,114 @@
 	lazygit = {
 	  enabled = true;
         };
+
+	dashboard = {
+	  enabled = true;
+	  preset = {
+	    keys = [
+	      {
+		icon = " ";
+		key = "f";
+		desc = "Find File";
+		action = "<leader>ff";
+	      }
+	      {
+		icon = " ";
+		key = "n";
+		desc = "New File";
+		action = ":ene | startinsert";
+	      }
+	      {
+		icon = " ";
+		key = "w";
+		desc = "Find Text";
+		action = "<leader>fw";
+	      }
+	      {
+		icon = " ";
+		key = "r";
+		desc = "Recent Files";
+		action = "<leader>fr";
+	      }
+	      {
+		icon = " ";
+		key = "s";
+		desc = "Restore Session";
+		action = "<leader>Ss";
+	      }
+	      {
+		icon = "";
+		key = "g";
+		desc = "LazyGit";
+		action = "<leader>gg";
+	      }
+	      {
+		icon = " ";
+		key = "b";
+		desc = "Browse Repo";
+		action = "<leader>gB";
+	      }
+	      {
+		icon = " ";
+		key = "q";
+		desc = "Quit";
+		action = ":qa";
+	      }
+	    ];
+	  };
+	  sections = [
+	    {
+	      section = "header";
+	      padding = 1;
+	    }
+	    {
+	      icon = " ";
+	      title = "Keymaps";
+	      section = "keys";
+	      gap = 1;
+	      padding = 1;
+	      indent = 3;
+	    }
+	    {
+	      icon = " ";
+	      title = "Recent Files";
+	      section = "recent_files";
+	      padding = 1;
+	      indent = 3;
+	    }
+	    {
+	      icon = " ";
+	      title = "Projects";
+	      section = "projects";
+	      padding = 1;
+	      indent = 3;
+	    }
+	    (commonPane2Section // {
+	      icon = " ";
+	      title = "Git Status";
+	      cmd = "${pkgs.hub}/bin/hub status --short --branch --renames";
+	      height = 5;
+	    })
+	    (commonPane2Section // {
+	      icon = " ";
+	      title = "Notifications";
+	      cmd = "gh notify -s -a -n5";
+	      height = 5;
+	    })
+	    (commonPane2Section // {
+	      icon = " ";
+	      title = "Open PRs";
+	      cmd = "gh pr list -L 3";
+	      height = 7;
+	    })
+	    (commonPane2Section // {
+	      icon = " ";
+	      title = "Open Issues";
+	      cmd = "gh issue list -L 3";
+	      height = 7;
+	    })
+	  ];
+	};
       };
     };
 
