76321f93d93f58d9626577f3c2f274f475cc6e4d
Xamcost <maxime.costalonga@gmail.com>
2025-03-26 00:14:57 +0000

    feat(glance): add services

--- services/glance.nix
+++ services/glance.nix
@@ -11,6 +11,13 @@
     "glance/rss01"= {};
 
     "glance/subreddit01" = {};
+
+    "glance/svc01" = {};
+    "glance/svc02" = {};
+    "glance/svc03" = {};
+    "glance/svc04" = {};
+    "glance/svc05" = {};
+    "glance/svc06" = {};
   };
 
   sops.templates."glance.env".content = ''
@@ -22,6 +29,12 @@
     YT06 = ${config.sops.placeholder."glance/yt06"}
     RSS01 = ${config.sops.placeholder."glance/rss01"}
     SUBREDDIT01 = ${config.sops.placeholder."glance/subreddit01"}
+    SVC01 = ${config.sops.placeholder."glance/svc01"}
+    SVC02 = ${config.sops.placeholder."glance/svc02"}
+    SVC03 = ${config.sops.placeholder."glance/svc03"}
+    SVC04 = ${config.sops.placeholder."glance/svc04"}
+    SVC05 = ${config.sops.placeholder."glance/svc05"}
+    SVC06 = ${config.sops.placeholder."glance/svc06"}
   '';
 
   services.glance = {
@@ -101,6 +114,73 @@
                     "\${YT04}"
                     "\${YT05}"
                     "\${YT06}"
+                  ];
+                }
+              ];
+            }
+            {
+              size = "small";
+              widgets = [
+                {
+                  type = "monitor";
+                  title = "Services";
+                  sites = [
+                    {
+                      title = "Home Assistant";
+                      url = "\${SVC01}";
+                      icon = "di:home-assistant";
+                    }
+                    {
+                      title = "Grafana";
+                      url = "\${SVC02}";
+                      icon = "di:grafana";
+                    }
+                    {
+                      title = "Nextcloud";
+                      url = "\${SVC03}";
+                      icon = "di:nextcloud";
+                    }
+                    {
+                      title = "Immich";
+                      url = "\${SVC04}";
+                      icon = "di:immich";
+                    }
+                    {
+                      title = "AdGuard Home";
+                      url = "\${SVC05}";
+                      icon = "di:adguard-home";
+                    }
+                    {
+                      title = "Calibre";
+                      url = "\${SVC06}";
+                      icon = "di:calibre-web";
+                    }
+                  ];
+                }
+                {
+                  type = "server-stats";
+                  servers = [
+                    {
+                      type = "local";
+                      name = "Elysium";
+                      mountpoints = {
+                        "/" = {
+                          name = "root";
+                        };
+                        "/boot" = {
+                          name = "boot";
+                        };
+                        "/mnt/lethe" = {
+                          name = "lethe";
+                        };
+                        "/mnt/tartaros" = {
+                          name = "tartaros";
+                        };
+                        "/mnt/cocytos" = {
+                          name = "cocytos";
+                        };
+                      };
+                    }
                   ];
                 }
               ];
