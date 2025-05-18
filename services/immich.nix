a487c949575df79d0c23018829374f434de317ba
Xamcost <maxime.costalonga@gmail.com>
2025-05-18 16:38:30 +0100

    maint(immich): update to v1.132.3

--- services/immich.nix
+++ services/immich.nix
@@ -2,7 +2,7 @@
 { pkgs, lib, config, ... }:
 
 {
-  sops.secrets.immich-postgres-password = {};
+  sops.secrets.immich-postgres-password = { };
 
   sops.templates."immich.env".content = ''
     DB_DATABASE_NAME=immich
@@ -17,29 +17,18 @@
 
   # Containers
   virtualisation.oci-containers.containers."immich_server" = {
-    image = "ghcr.io/immich-app/immich-server:v1.123.0";
-    environment = {
-      TZ = "Europe/London";
-    };
-    environmentFiles = [
-      config.sops.templates."immich.env".path
-    ];
+    image = "ghcr.io/immich-app/immich-server:v1.132.3";
+    environment = { TZ = "Europe/London"; };
+    environmentFiles = [ config.sops.templates."immich.env".path ];
     volumes = [
       "/etc/localtime:/etc/localtime:ro"
       "/mnt/lethe/immich/app:/usr/src/app/upload:rw"
     ];
-    ports = [
-      "2283:2283/tcp"
-    ];
-    dependsOn = [
-      "immich_postgres"
-      "immich_redis"
-    ];
+    ports = [ "2283:2283/tcp" ];
+    dependsOn = [ "immich_postgres" "immich_redis" ];
     log-driver = "journald";
-    extraOptions = [
-      "--network-alias=immich-server"
-      "--network=immich_default"
-    ];
+    extraOptions =
+      [ "--network-alias=immich-server" "--network=immich_default" ];
   };
   systemd.services."docker-immich_server" = {
     serviceConfig = {
@@ -48,35 +37,19 @@
       RestartSec = lib.mkOverride 500 "100ms";
       RestartSteps = lib.mkOverride 500 9;
     };
-    after = [
-      "docker-network-immich_default.service"
-    ];
-    requires = [
-      "docker-network-immich_default.service"
-    ];
-    partOf = [
-      "docker-compose-immich-root.target"
-    ];
-    wantedBy = [
-      "docker-compose-immich-root.target"
-    ];
+    after = [ "docker-network-immich_default.service" ];
+    requires = [ "docker-network-immich_default.service" ];
+    partOf = [ "docker-compose-immich-root.target" ];
+    wantedBy = [ "docker-compose-immich-root.target" ];
   };
   virtualisation.oci-containers.containers."immich_machine_learning" = {
-    image = "ghcr.io/immich-app/immich-machine-learning:v1.123.0";
-    environment = {
-      TZ = "Europe/London";
-    };
-    environmentFiles = [
-      config.sops.templates."immich.env".path
-    ];
-    volumes = [
-      "immich_model-cache:/cache:rw"
-    ];
+    image = "ghcr.io/immich-app/immich-machine-learning:v1.132.3";
+    environment = { TZ = "Europe/London"; };
+    environmentFiles = [ config.sops.templates."immich.env".path ];
+    volumes = [ "immich_model-cache:/cache:rw" ];
     log-driver = "journald";
-    extraOptions = [
-      "--network-alias=immich-machine-learning"
-      "--network=immich_default"
-    ];
+    extraOptions =
+      [ "--network-alias=immich-machine-learning" "--network=immich_default" ];
   };
   systemd.services."docker-immich_machine_learning" = {
     serviceConfig = {
@@ -93,28 +66,34 @@
       "docker-network-immich_default.service"
       "docker-volume-immich_model-cache.service"
     ];
-    partOf = [
-      "docker-compose-immich-root.target"
-    ];
-    wantedBy = [
-      "docker-compose-immich-root.target"
-    ];
+    partOf = [ "docker-compose-immich-root.target" ];
+    wantedBy = [ "docker-compose-immich-root.target" ];
   };
   virtualisation.oci-containers.containers."immich_postgres" = {
-    image = "docker.io/tensorchord/pgvecto-rs:pg14-v0.2.0@sha256:90724186f0a3517cf6914295b5ab410db9ce23190a2d9d0b9dd6463e3fa298f0";
-    environment = {
-      POSTGRES_INITDB_ARGS = "--data-checksums";
-    };
-    environmentFiles = [
-      config.sops.templates."immich_db.env".path
+    image =
+      "docker.io/tensorchord/pgvecto-rs:pg14-v0.2.0@sha256:739cdd626151ff1f796dc95a6591b55a714f341c737e27f045019ceabf8e8c52";
+    environment = { POSTGRES_INITDB_ARGS = "--data-checksums"; };
+    environmentFiles = [ config.sops.templates."immich_db.env".path ];
+    volumes = [ "/mnt/lethe/immich/postgres:/var/lib/postgresql/data:rw" ];
+    cmd = [
+      "postgres"
+      "-c"
+      "shared_preload_libraries=vectors.so"
+      "-c"
+      ''search_path="$user", public, vectors''
+      "-c"
+      "logging_collector=on"
+      "-c"
+      "max_wal_size=2GB"
+      "-c"
+      "shared_buffers=512MB"
+      "-c"
+      "wal_compression=on"
     ];
-    volumes = [
-      "/mnt/lethe/immich/postgres:/var/lib/postgresql/data:rw"
-    ];
-    cmd = [ "postgres" "-c" "shared_preload_libraries=vectors.so" "-c" "search_path=\"$user\", public, vectors" "-c" "logging_collector=on" "-c" "max_wal_size=2GB" "-c" "shared_buffers=512MB" "-c" "wal_compression=on" ];
     log-driver = "journald";
     extraOptions = [
-      "--health-cmd=pg_isready --dbname='immich' --username='postgres' || exit 1; Chksum=\"$(psql --dbname='immich' --username='postgres' --tuples-only --no-align --command='SELECT COALESCE(SUM(checksum_failures), 0) FROM pg_stat_database')\"; echo \"checksum failure count is $Chksum\"; [ \"$Chksum\" = '0' ] || exit 1"
+      ''
+        --health-cmd=pg_isready --dbname='immich' --username='postgres' || exit 1; Chksum="$(psql --dbname='immich' --username='postgres' --tuples-only --no-align --command='SELECT COALESCE(SUM(checksum_failures), 0) FROM pg_stat_database')"; echo "checksum failure count is $Chksum"; [ "$Chksum" = '0' ] || exit 1''
       "--health-interval=5m0s"
       "--health-start-period=5m0s"
       "--network-alias=database"
@@ -128,21 +107,14 @@
       RestartSec = lib.mkOverride 500 "100ms";
       RestartSteps = lib.mkOverride 500 9;
     };
-    after = [
-      "docker-network-immich_default.service"
-    ];
-    requires = [
-      "docker-network-immich_default.service"
-    ];
-    partOf = [
-      "docker-compose-immich-root.target"
-    ];
-    wantedBy = [
-      "docker-compose-immich-root.target"
-    ];
+    after = [ "docker-network-immich_default.service" ];
+    requires = [ "docker-network-immich_default.service" ];
+    partOf = [ "docker-compose-immich-root.target" ];
+    wantedBy = [ "docker-compose-immich-root.target" ];
   };
   virtualisation.oci-containers.containers."immich_redis" = {
-    image = "docker.io/redis:6.2-alpine@sha256:2d1463258f2764328496376f5d965f20c6a67f66ea2b06dc42af351f75248792";
+    image =
+      "docker.io/valkey/valkey:8-bookworm@sha256:ff21bc0f8194dc9c105b769aeabf9585fea6a8ed649c0781caeac5cb3c247884";
     log-driver = "journald";
     extraOptions = [
       "--health-cmd=redis-cli ping || exit 1"
@@ -157,18 +129,10 @@
       RestartSec = lib.mkOverride 500 "100ms";
       RestartSteps = lib.mkOverride 500 9;
     };
-    after = [
-      "docker-network-immich_default.service"
-    ];
-    requires = [
-      "docker-network-immich_default.service"
-    ];
-    partOf = [
-      "docker-compose-immich-root.target"
-    ];
-    wantedBy = [
-      "docker-compose-immich-root.target"
-    ];
+    after = [ "docker-network-immich_default.service" ];
+    requires = [ "docker-network-immich_default.service" ];
+    partOf = [ "docker-compose-immich-root.target" ];
+    wantedBy = [ "docker-compose-immich-root.target" ];
   };
 
   # Networks
@@ -204,9 +168,7 @@
   # When started, this will automatically create all resources and start
   # the containers. When stopped, this will teardown all resources.
   systemd.targets."docker-compose-immich-root" = {
-    unitConfig = {
-      Description = "Root target generated by compose2nix.";
-    };
+    unitConfig = { Description = "Root target generated by compose2nix."; };
     wantedBy = [ "multi-user.target" ];
   };
 }
