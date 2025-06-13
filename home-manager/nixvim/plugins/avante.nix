c18e418c1a753770ce895cdce4e46c6ca9568374
xamcost <xamcost@xam.simplelogin.com>
2025-06-13 02:09:06 +0100

    feat(nixvim): add ollama to avante

--- home-manager/nixvim/plugins/avante.nix
+++ home-manager/nixvim/plugins/avante.nix
@@ -6,15 +6,29 @@ in {
       enable = isEnabled;
 
       settings = {
-        provider = "copilot";
-        copilot = {
-          endpoint = "https://api.githubcopilot.com";
-          # model = "gpt-4o-2024-08-06";
-          # model = "claude-3.7-sonnet";
-          allow_insecure = false; # Allow insecure server connections
-          timeout = 30000; # Timeout in milliseconds
-          temperature = 0;
-          max_tokens = 4096;
+        # provider = "copilot";
+        provider = "ollama";
+        providers = {
+          copilot = {
+            endpoint = "https://api.githubcopilot.com";
+            # model = "gpt-4o-2024-08-06";
+            # model = "claude-3.7-sonnet";
+            allow_insecure = false; # Allow insecure server connections
+            timeout = 30000; # Timeout in milliseconds
+            extra_request_body = {
+              options = {
+                temperature = 0;
+                max_tokens = 4096;
+              };
+
+            };
+          };
+          ollama = {
+            model = "llama3.2";
+            # model = "deepseek-r1";
+            # model = "qwen2.5-coder";
+            endpoint = "http://127.0.0.1:11434"; # no /v1 at the end.
+          };
         };
         dual_boost = { enabled = false; };
         behaviour = {
