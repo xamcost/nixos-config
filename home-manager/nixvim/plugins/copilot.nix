8d96fc905565b1afa165f1e0afd765a838047b10
xamcost <xamcost@xam.simplelogin.com>
2025-10-14 15:18:10 +0100

    feat(nixvim): exclude env files from copilot

--- home-manager/nixvim/plugins/copilot.nix
+++ home-manager/nixvim/plugins/copilot.nix
@@ -1,36 +1,47 @@
 { homeConfigName, ... }:
 let
-  isEnabled = !builtins.elem homeConfigName [
-    "xam@aeneas"
-  ];
+  isEnabled =
+    !builtins.elem homeConfigName [
+      "xam@aeneas"
+    ];
 in
 {
   programs.nixvim = {
     plugins.copilot-lua = {
       enable = isEnabled;
       settings = {
-	panel = { enabled = false; };
-	suggestion = {
-	  enabled = true;
-	  auto_trigger = true;
-	  keymap = {
-	    accept = "<C-l>";
-	    acceptWord = false;
-	    acceptLine = false;
-	    next = "<M-]>";
-	    prev = "<M-[>";
-	    dismiss = "<M-Bslash>";
-	    # next = "<C-.>";
-	    # prev = "<C-,>";
-	    # dismiss = "<C/>";
-	  };
-	};
-	filetypes = {
-	  markdown = true;
-	  yaml = true;
-	};
+        panel = {
+          enabled = false;
+        };
+        suggestion = {
+          enabled = true;
+          auto_trigger = true;
+          keymap = {
+            accept = "<C-l>";
+            acceptWord = false;
+            acceptLine = false;
+            next = "<M-]>";
+            prev = "<M-[>";
+            dismiss = "<M-Bslash>";
+            # next = "<C-.>";
+            # prev = "<C-,>";
+            # dismiss = "<C/>";
+          };
+        };
+        filetypes = {
+          markdown = true;
+          yaml = true;
+          sh.__raw = ''
+            function()
+              if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+                -- disable for .env files
+                return false
+              end
+              return true
+            end
+          '';
+        };
       };
     };
   };
 }
-
