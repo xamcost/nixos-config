b5fde5e4e62f8a8f49c30451527b71b65162a06c
xamcost <xamcost@xam.simplelogin.com>
2025-10-18 21:53:39 +0100

    feat(opencode): add context7

--- home-manager/opencode/default.nix
+++ home-manager/opencode/default.nix
@@ -4,6 +4,12 @@
     settings = {
       theme = "tokyonight";
       autoupdate = true;
+      mcp = {
+        context7 = {
+          type = "remote";
+          url = "https://mcp.context7.com/mcp";
+        };
+      };
     };
   };
 }
