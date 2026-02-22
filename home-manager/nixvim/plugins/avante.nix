7ab6bf4a94c915fc714eae802227faef3a6a3de2
xamcost <xamcost@xam.simplelogin.com>
2026-02-22 09:02:03 +0000

    maint(nixvim): reenable avante

--- home-manager/nixvim/plugins/avante.nix
+++ home-manager/nixvim/plugins/avante.nix
@@ -4,8 +4,8 @@
   ...
 }:
 let
-  # isEnabled = !builtins.elem homeConfigName [ "xam@aeneas" ];
-  isEnabled = false;
+  isEnabled = builtins.elem homeConfigName [ "maximecostalonga@xam-mac-m4" ];
+  # isEnabled = false;
 in
 {
   programs.nixvim = {
@@ -14,26 +14,25 @@ in
 
       settings = {
         provider = "copilot";
-        # provider = "ollama";
+        mode = "agentic"; # or legacy
+        auto_suggestions_provider = "ollama";
         providers = {
           copilot = {
             endpoint = "https://api.githubcopilot.com";
-            # model = "gpt-4o-2024-08-06";
+            model = "gpt-4.1";
             # model = "claude-3.7-sonnet";
             allow_insecure = false; # Allow insecure server connections
             timeout = 30000; # Timeout in milliseconds
             extra_request_body = {
               options = {
-                temperature = 0;
-                max_tokens = 4096;
+                temperature = 0.1;
+                max_tokens = 20480;
               };
 
             };
           };
           ollama = {
-            model = "llama3.2";
-            # model = "deepseek-r1";
-            # model = "qwen2.5-coder";
+            model = "deepseek-coder-v2:16b";
             endpoint = "http://127.0.0.1:11434"; # no /v1 at the end.
           };
         };
