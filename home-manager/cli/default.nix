d10716029f574429871f5fbbb1ba7463bfe2b100
xamcost <xamcost@xam.simplelogin.com>
2026-02-27 00:25:27 +0000

    fix(fzf): restrict preview to file widget

--- home-manager/cli/default.nix
+++ home-manager/cli/default.nix
@@ -30,7 +30,7 @@
       enable = true;
       enableZshIntegration = true;
       defaultCommand = "fd --type f";
-      defaultOptions = [ "--preview 'bat --color=always {}'" ];
+      fileWidgetOptions = [ "--preview 'bat --color=always {}'" ];
     };
 
     ripgrep = {
