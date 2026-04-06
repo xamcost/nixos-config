2b30063113bbc690b35cb6c86857c22fb89f2df8
xamcost <xamcost@xam.simplelogin.com>
2026-04-06 01:02:34 +0100

    maint(elysium): minor reformatting of signal

--- services/signal.nix
+++ services/signal.nix
@@ -2,9 +2,10 @@
   virtualisation.oci-containers.containers."signal" = {
     image = "bbernhard/signal-cli-rest-api:latest";
     autoStart = true;
-    volumes =
-      [ "/var/run/signal/signal-cli-config:/home/.local/share/signal-cli" ];
-    environment = { MODE = "normal"; };
+    volumes = [ "/var/run/signal/signal-cli-config:/home/.local/share/signal-cli" ];
+    environment = {
+      MODE = "normal";
+    };
     ports = [ "8091:8080" ];
   };
 }
