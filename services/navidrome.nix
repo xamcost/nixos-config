0bbfc046723ead7570a5d5da6b26c8e906918ed8
xamcost <xamcost@xam.simplelogin.com>
2026-05-10 02:33:25 +0100

    maint(elysium): store temp fix for navidrome

--- services/navidrome.nix
+++ services/navidrome.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   sops.secrets.domain = { };
   sops.secrets."navidrome/password-encryption-key" = { };
@@ -13,6 +13,16 @@
     ND_LASTFM_SECRET=${config.sops.placeholder."navidrome/lastfm-secret"}
     ND_PASSWORDENCRYPTIONKEY=${config.sops.placeholder."navidrome/password-encryption-key"}
   '';
+
+  # Temporary fix: https://github.com/NixOS/nixpkgs/issues/481611#issuecomment-3890134071
+  # nixpkgs.overlays = [
+  #   (self: super: {
+  #     navidrome = self.callPackage (pkgs.fetchurl {
+  #       url = "https://raw.githubusercontent.com/cimm/nixpkgs/71aa374ad541b41e6fccd543c67b6952d2ccafca/pkgs/by-name/na/navidrome/package.nix";
+  #       sha256 = "16mfj85w8d7vzc9pgcgjn7a71z7jywqpdn8igk9zp0hw9dvm9rmq";
+  #     }) { };
+  #   })
+  # ];
 
   services.navidrome = {
     enable = true;
